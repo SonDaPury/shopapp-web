09c1b07e93fa542ee90e25599ba2819493e97de8
Son Dong Xuan <sondongxuan1205@gmail.com>
2025-01-08 21:22:55 +0700

    learn nest js

--- shopapp-server/src/app.module.ts
+++ shopapp-server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExampleControllerModule } from './controllers/example-controller/example-controller.module';
+import { ExampleControllerController } from './example-controller/example-controller.controller';
+import { ExampleControllerModule } from './controllers/example-controller/example-controller.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ExampleControllerModule],
+  controllers: [AppController, ExampleControllerController],
   providers: [AppService],
 })
 export class AppModule {}
